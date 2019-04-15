c1d18251374b7309ff0c86d68c2fc145b5048133
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-04-15 15:38:53 -0400

    vae importance sampling (not working yet)

--- assignment3/vae.py
+++ assignment3/vae.py
@@ -19,6 +19,16 @@ class VAE(nn.Module):
         self.encoder_activation3 = nn.ELU()
         self.encoder_linear_mean = nn.Linear(in_features=256, out_features=100)
         self.encoder_linear_var = nn.Linear(in_features=256, out_features=100)
+        self.encoder_sequence = nn.Sequential(
+            self.encoder_conv1,
+            self.encoder_activation1,
+            self.encoder_pool1,
+            self.encoder_conv2,
+            self.encoder_activation2,
+            self.encoder_pool2,
+            self.encoder_conv3,
+            self.encoder_activation3
+        )
 
         # Decoder
         self.decoder_linear = nn.Linear(in_features=100, out_features=256)
@@ -32,18 +42,20 @@ class VAE(nn.Module):
         self.decoder_conv3 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3, 3), padding=(2, 2))
         self.decoder_activation4 = nn.ELU()
         self.decoder_conv4 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=(3, 3), padding=(2, 2))
+        self.decoder_sequence = nn.Sequential(
+            self.decoder_conv1,
+            self.decoder_activation2,
+            self.decoder_upsampling1,
+            self.decoder_conv2,
+            self.decoder_activation3,
+            self.decoder_upsampling2,
+            self.decoder_conv3,
+            self.decoder_activation4,
+            self.decoder_conv4
+        )
 
     def encode(self, x):
-        h = nn.Sequential(
-            self.encoder_conv1,
-            self.encoder_activation1,
-            self.encoder_pool1,
-            self.encoder_conv2,
-            self.encoder_activation2,
-            self.encoder_pool2,
-            self.encoder_conv3,
-            self.encoder_activation3
-        )(x)
+        h = self.encoder_sequence(x)
         h = torch.squeeze(h)
         z_mu = self.encoder_linear_mean(h)
         z_sig2 = self.encoder_linear_var(h)
@@ -58,17 +70,7 @@ class VAE(nn.Module):
         h = self.decoder_linear(z)
         h = self.decoder_activation1(h)
         h = torch.unsqueeze(torch.unsqueeze(h, -1), -1)
-        h = nn.Sequential(
-            self.decoder_conv1,
-            self.decoder_activation2,
-            self.decoder_upsampling1,
-            self.decoder_conv2,
-            self.decoder_activation3,
-            self.decoder_upsampling2,
-            self.decoder_conv3,
-            self.decoder_activation4,
-            self.decoder_conv4
-        )(h)
+        h = self.decoder_sequence(h)
         o = torch.sigmoid(h)
         return o
 
