1d7ee6bd32682ed7755f2fca4af7366478addf86
Alexandre Chartrand <alexchartrand91@gmail.com>
2019-04-15 14:16:49 -0400

    Add gp to gan

--- assignment3/GAN.py
+++ assignment3/GAN.py
@@ -1,21 +1,10 @@
 import torch
 from torch import nn
-from torch import functional as f
-from torch.autograd import  Variable
+from torch.autograd import  Variable, grad
 from torch import optim
 from classify_svhn import get_data_loader
 
-import matplotlib.pyplot as plt
-
-
 # This code is inspired by: https://github.com/wiseodd/generative-models/blob/master/GAN/wasserstein_gan/wgan_pytorch.py
-
-lr = 0.00005
-batch_size = 16
-cliping = 0.01
-z_size = 100
-im_size = 32
-n_critic = 5
 
 class Flatten(nn.Module):
     def forward(self, x):
@@ -24,7 +13,7 @@ class Flatten(nn.Module):
 
 class Generator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, z_size):
         super(Generator, self).__init__()
         self.linear = nn.Linear(in_features=z_size, out_features=256)
         self.linear_activation = nn.ELU()
@@ -41,68 +30,83 @@ class Generator(nn.Module):
             nn.Conv2d(in_channels=16, out_channels=3, kernel_size=(3, 3), padding=(1, 1))
         )
 
+        self.output_activation = nn.Tanh()
+
     def forward(self, z):
         h = self.linear(z)
         h = self.linear_activation(h)
         h = torch.unsqueeze(torch.unsqueeze(h, -1), -1)
 
         h = self.conv_stack(h)
-        return h
+        return self.output_activation(h)
 
-    @staticmethod
-    def loss(fake):
+    def loss(self, fake):
         return -torch.mean(fake)
 
 
 class Discriminator(nn.Module):
 
-    def __init__(self):
+    def __init__(self, im_size, device):
         super(Discriminator, self).__init__()
 
         flatten_size = im_size*im_size*3
         self.mlp_stack = nn.Sequential(
             Flatten(),
-            nn.Linear(in_features=flatten_size, out_features=256),
+            nn.Linear(in_features=flatten_size, out_features=512),
             nn.ReLU(),
-            nn.Linear(in_features=256, out_features=64),
+            nn.Linear(in_features=512, out_features=512),
             nn.ReLU(),
-            nn.Linear(in_features=64, out_features=1)
+            nn.Linear(in_features=512, out_features=1)
         )
 
+        self.device = device
 
     def forward(self, x):
         return self.mlp_stack(x)
 
-    def clipWeights(self):
+    def clipWeights(self, c):
         for w in self.parameters():
-            w.data.clamp_(-cliping, cliping)
+            w.data.clamp_(-c, c)
 
-    @staticmethod
-    def loss(real, fake):
-        return -torch.mean(real).sub(torch.mean(fake))
+    def gradientPenality(self, real, fake):
+        shape = [real.size(0)] + [1] * (real.dim() - 1)
+        e = Variable(torch.rand(shape, device=self.device))
 
-def showImg(x):
-    x = x.permute(1, 2, 0)
-    plt.imshow((x.numpy() * 0.5) + 0.5)
+        x_hat = e*real + (1-e)*fake
+        x_hat = Variable(x_hat, requires_grad=True).to(device)
+        d_hat = self(x_hat)
+        d_grad = grad(d_hat, x_hat, torch.ones(d_hat.shape, device=self.device), create_graph=True)[0].view(x_hat.size(0), -1)
 
+        return torch.mean((d_grad.norm(p=2, dim=1) - 1)**2)
+
+    def loss(self, real, fake):
+        d_real = self(real)
+        d_fake = self(fake)
+        w_distance = torch.mean(d_real).sub(torch.mean(d_fake))
+
+        gp = self.gradientPenality(real, fake)
+        return -w_distance + 10*gp
 
 
 if __name__ == "__main__":
+    lr = 0.00005
+    batch_size = 16
+    cliping = 0.01
+    z_size = 100
+    im_size = 32
+    n_critic = 5
+
     device = torch.device("cpu")
 
-    cuda = torch.cuda.is_available()
-    if False:
-        device = torch.device("cuda")
+    g = Generator(z_size).to(device)
+    d = Discriminator(im_size, device).to(device)
 
-    g = Generator().to(device)
-    d = Discriminator().to(device)
-
-    g_optim = optim.RMSprop(g.parameters(), lr=lr)
-    d_optim = optim.RMSprop(d.parameters(), lr=lr)
+    g_optim = optim.Adam(g.parameters(), lr=lr)
+    d_optim = optim.Adam(d.parameters(), lr=lr)
 
     train, valid, test = get_data_loader("svhn", batch_size)
 
-    for i in range(10000):
+    for i in range(10):
 
         # Train more the dicriminator
         for j in range(n_critic):
@@ -122,11 +126,9 @@ if __name__ == "__main__":
 
             real_sample = real_sample.to(device)
 
-            d_loss = d.loss(d(real_sample), d(fake_sample))
+            d_loss = d.loss(real_sample, fake_sample)
             d_loss.backward()
             d_optim.step()
-
-            d.clipWeights()
 
         # Train the generator
         d_optim.zero_grad()
