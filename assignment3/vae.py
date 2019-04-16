cb7238fcb1cfde280590352ade7e2ae3df4cf47e
Raphael Royer-Rivard <raphael.royer.rivard@gmail.com>
2019-04-16 15:15:51 -0400

    updated VAE for q3 and included the training of VAE in the q3 notebook

--- assignment3/vae.py
+++ assignment3/vae.py
@@ -3,56 +3,97 @@ import torch
 import torch.utils.data
 from torch import nn
 
+# This code is inspired by: https://github.com/pytorch/examples/blob/master/vae/main.py
+# and: https://github.com/bjlkeng/sandbox/tree/master/notebooks/variational_autoencoder-svhn
+
 
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, latent_size=100, type='MNIST'):
         super(VAE, self).__init__()
+        self.type = type
 
-        # Encoder
-        self.encoder_conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3))
-        self.encoder_activation1 = nn.ELU()
-        self.encoder_pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.encoder_conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3))
-        self.encoder_activation2 = nn.ELU()
-        self.encoder_pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.encoder_conv3 = nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(5, 5))
-        self.encoder_activation3 = nn.ELU()
-        self.encoder_linear_mean = nn.Linear(in_features=256, out_features=100)
-        self.encoder_linear_var = nn.Linear(in_features=256, out_features=100)
-        self.encoder_sequence = nn.Sequential(
-            self.encoder_conv1,
-            self.encoder_activation1,
-            self.encoder_pool1,
-            self.encoder_conv2,
-            self.encoder_activation2,
-            self.encoder_pool2,
-            self.encoder_conv3,
-            self.encoder_activation3
-        )
+        if type == 'MNIST':
+            # Encoder
+            self.encoder_sequence = nn.Sequential(
+                # layer 1
+                nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3)),
+                nn.ELU(),
+                nn.AvgPool2d(kernel_size=2, stride=2),
+                # layer 2
+                nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3)),
+                nn.ELU(),
+                nn.AvgPool2d(kernel_size=2, stride=2),
+                # layer 3
+                nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(5, 5)),
+                nn.ELU()
+            )
+            self.encoder_linear_mean = nn.Linear(in_features=256, out_features=latent_size)
+            self.encoder_linear_var = nn.Linear(in_features=256, out_features=latent_size)
 
-        # Decoder
-        self.decoder_linear = nn.Linear(in_features=100, out_features=256)
-        self.decoder_activation1 = nn.ELU()
-        self.decoder_conv1 = nn.Conv2d(in_channels=256, out_channels=64, kernel_size=(5, 5), padding=(4, 4))
-        self.decoder_activation2 = nn.ELU()
-        self.decoder_upsampling1 = nn.UpsamplingBilinear2d(scale_factor=2)
-        self.decoder_conv2 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3, 3), padding=(2, 2))
-        self.decoder_activation3 = nn.ELU()
-        self.decoder_upsampling2 = nn.UpsamplingBilinear2d(scale_factor=2)
-        self.decoder_conv3 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3, 3), padding=(2, 2))
-        self.decoder_activation4 = nn.ELU()
-        self.decoder_conv4 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=(3, 3), padding=(2, 2))
-        self.decoder_sequence = nn.Sequential(
-            self.decoder_conv1,
-            self.decoder_activation2,
-            self.decoder_upsampling1,
-            self.decoder_conv2,
-            self.decoder_activation3,
-            self.decoder_upsampling2,
-            self.decoder_conv3,
-            self.decoder_activation4,
-            self.decoder_conv4
-        )
+            # Decoder
+            # layer 1
+            self.decoder_linear = nn.Linear(in_features=latent_size, out_features=256)
+            self.decoder_activation1 = nn.ELU()
+            self.decoder_sequence = nn.Sequential(
+                # layer 2
+                nn.Conv2d(in_channels=256, out_channels=64, kernel_size=(5, 5), padding=(4, 4)),
+                nn.ELU(),
+                nn.UpsamplingBilinear2d(scale_factor=2),
+                # layer 3
+                nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3, 3), padding=(2, 2)),
+                nn.ELU(),
+                nn.UpsamplingBilinear2d(scale_factor=2),
+                # layer 4
+                nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3, 3), padding=(2, 2)),
+                nn.ELU(),
+                # layer 5
+                nn.Conv2d(in_channels=16, out_channels=1, kernel_size=(3, 3), padding=(2, 2))
+            )
+            self.decoder_output_activation = nn.Sigmoid()
+
+        else:
+            input_size = 3072
+            hidden_size = 2048
+            dropout_p = 0.2
+            # Encoder
+            self.encoder_sequence = nn.Sequential(
+                # input
+                nn.BatchNorm1d(input_size),
+                # layer 1
+                nn.Linear(in_features=input_size, out_features=hidden_size),
+                nn.BatchNorm1d(hidden_size),
+                nn.ReLU(),
+                nn.Dropout(p=dropout_p),
+                # layer 2
+                nn.Linear(in_features=hidden_size, out_features=hidden_size),
+                nn.BatchNorm1d(hidden_size),
+                nn.ReLU(),
+                nn.Dropout(p=dropout_p),
+                # layer 3
+                nn.Linear(in_features=hidden_size, out_features=hidden_size),
+                nn.BatchNorm1d(hidden_size),
+                nn.ReLU(),
+                nn.Dropout(p=dropout_p)
+            )
+            self.encoder_linear_mean = nn.Linear(in_features=hidden_size, out_features=latent_size)
+            self.encoder_linear_var = nn.Linear(in_features=hidden_size, out_features=latent_size)
+
+            # Decoder (same as GAN generator)
+            self.decoder_linear = nn.Linear(in_features=latent_size, out_features=256)
+            self.decoder_activation1 = nn.ELU()
+
+            self.decoder_sequence = nn.Sequential(
+                nn.Conv2d(in_channels=256, out_channels=64, kernel_size=(3, 3), padding=(4, 4)),
+                nn.ELU(),
+                nn.UpsamplingBilinear2d(scale_factor=2),
+                nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(3, 3), padding=(2, 2)),
+                nn.ELU(),
+                nn.UpsamplingBilinear2d(scale_factor=2),
+                nn.Conv2d(in_channels=32, out_channels=16, kernel_size=(3, 3), padding=(1, 1)),
+                nn.ELU(),
+                nn.Conv2d(in_channels=16, out_channels=3, kernel_size=(3, 3), padding=(1, 1))
+            )
+            self.decoder_output_activation = nn.Tanh()
 
     def encode(self, x):
         h = self.encoder_sequence(x)
@@ -71,7 +112,7 @@ class VAE(nn.Module):
         h = self.decoder_activation1(h)
         h = torch.unsqueeze(torch.unsqueeze(h, -1), -1)
         h = self.decoder_sequence(h)
-        o = torch.sigmoid(h)
+        o = self.decoder_output_activation(h)
         return o
 
     def forward(self, x):
